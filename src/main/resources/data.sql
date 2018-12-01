3f3a4774d77ac2111784bfbd8593dff24becb949
lincoln <lincoln.ufop@gmail.com>
2018-12-01 14:17:43 -0200

    Carga bd e validacao do teste get

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO post VALUES(1001, '2018-12-01', 'Primeira descricao para testes api', 'Titulo 1');
+INSERT INTO post VALUES(1002, '2018-12-02', 'Segunda descricao para testes api', 'Titulo 2');
