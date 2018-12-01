6d318dc2ce653c5d4ff3ca3d9ce6b2b38a23d6f1
lincoln <lincoln.ufop@gmail.com>
2018-12-01 12:30:36 -0200

    Criacao entidade Post

---
+++ src/main/java/net/superbid/entity/Post.java
@@ -0,0 +1,46 @@
+package net.superbid.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private LocalDate dataPublicacao;
+	private String titulo;
+	private String descricao;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public LocalDate getDataPublicacao() {
+		return dataPublicacao;
+	}
+	public void setDataPublicacao(LocalDate dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+}
