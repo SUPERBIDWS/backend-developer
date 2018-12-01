f8f511012e476079fb43c11cc7a9124ebe72d8e0
lincoln <lincoln.ufop@gmail.com>
2018-12-01 12:25:12 -0200

    Commit inicial

---
+++ src/main/java/net/superbid/BackendDeveloperApplication.java
@@ -0,0 +1,12 @@
+package net.superbid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeveloperApplication.class, args);
+	}
+}
